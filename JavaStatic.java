afc49eaf84b36d06437d40662e4bf15c61cdf40c
Dhinagar492 <115131623+Dhinagar492@users.noreply.github.com>
2022-10-10 15:48:56 +0530

    Add files via upload

---
+++ JavaStatic.java
@@ -0,0 +1,15 @@
+class JavaStatic{
+static String course="java";
+static void notes()
+{
+int a=10;
+int b=20;
+System.out.println(a);
+}
+public static void main(String[]args){
+//JavaStatic cal=new JavaStatic;
+//cal.notes;
+System.out.println(JavaStatic.course);
+JavaStatic.notes();
+}
+}
